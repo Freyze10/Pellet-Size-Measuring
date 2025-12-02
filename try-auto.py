7c8e30f2a0e8c04bf93557bf72c03e5053774260
Freyze10 <bjabillanoza@gmail.com>
2025-12-02 13:45:18 +0800

    adjusted height compensation

--- try-auto.py
+++ try-auto.py
@@ -19,7 +19,7 @@ EXCLUSION_THRESHOLD = 1.0
 
 # --- HEIGHT COMPENSATION (3mm pellet height vs flat ruler) ---
 # Simple cheat: pellets are 3mm higher than ruler, so they appear ~1.5% larger in perspective
-HEIGHT_COMPENSATION_FACTOR = 2.015  # Compensate for 3mm height difference
+HEIGHT_COMPENSATION_FACTOR = 1.215  # Compensate for 3mm height difference
 
 # --- CALIBRATION MODES ---
 CALIBRATION_MODE = 'CM'  # Options: 'CM' or 'INCH'
